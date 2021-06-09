d0c1cca5a06b7dde11699e07f1a82e6edc1acc63
Vladislav <moiseenkov-v@mail.ru>
2021-06-09 12:16:06 +0300

    devision in several files

--- gui/creategui.py
+++ gui/creategui.py
@@ -8,14 +8,10 @@ import csv
 import numpy as np
 
 path = clean_data_path
-tmp_columns = [
-    "Название", "Автор", "Средний", 'Язык', 'Количество', 'Количество рейтингов','Количество отзывов', 'Дата публикации',
-    'Издательство', 'Год публикации', 'Век публикации','Большая ли она']
-
 tmp_data = pd.read_csv('../data/clean_data.csv')
 
 
-def show():
+def show(frame22,combo_plot1,combo_plot2):
     if combo_plot1.get() != 'choose':
         for widgets in frame22.winfo_children():
             widgets.destroy()
@@ -37,13 +33,12 @@ def show():
 
 
 
-def download():
+def download(frame3):
     scrollbarx = Scrollbar(frame3, orient=HORIZONTAL)
     scrollbary = Scrollbar(frame3, orient=VERTICAL)
     columns = (
     "title", "authors", "average_rating", 'language_code', 'num_pages', 'ratings_count','text_reviews_count', 'publication_date',
     'publisher', 'pub_year', 'century')
-
     tree = ttk.Treeview(frame3, columns=columns, selectmode="extended",height = 15)
     scrollbary.config(command=tree.yview)
     scrollbary.pack(side = RIGHT, fill=Y)
@@ -76,11 +71,7 @@ def download():
             title, authors, average_rating, language_code, num_pages,ratings_count, text_reviews_count, publication_date, publisher,
             pub_year, century))
 
-def check(av2,pag2,rate2,rew2):
-    result = (np.array([av2,pag2,rate2,rew2]) !=0)
-    return result
-
-def reload():
+def reload(frame3,var_2,var_3,var_4,var_5,var_6,var_7,var_8,var_9):
     av1 = float(var_2.get())
     av2 = float(var_3.get())
     pag1 = int(var_4.get())
@@ -89,7 +80,6 @@ def reload():
     rate2 = float(var_7.get())
     rew1 = int(var_8.get())
     rew2 = int(var_9.get())
-    chk = check(av2,pag2,rate2,rew2)
     newcsv = []
     with open(clean_data_path) as f:
         reader = csv.reader(f, delimiter=',', quotechar='|')
@@ -108,118 +98,115 @@ def reload():
                 spamwriter.writerow(str)
     global tmp_data
     tmp_data = pd.read_csv('../data/reload.csv')
-    # tmp_data.rename(columns=tmp_columns)
-    # print(tmp_columns)
-    # print(tmp_data.columns)
-def download_main():
+
+def download_main(frame3):
     for widgets in frame3.winfo_children():
         widgets.destroy()
     global path
     path = clean_data_path
-    download()
+    download(frame3)
 
-def update_table():
+def update_table(frame3,var_2,var_3,var_4,var_5,var_6,var_7,var_8,var_9):
     for widgets in frame3.winfo_children():
         widgets.destroy()
     global path
-    reload()
+    reload(frame3,var_2,var_3,var_4,var_5,var_6,var_7,var_8,var_9)
     path = '../data/reload.csv'
-    download()
+    download(frame3)
 
 
-
-root = Tk()
-root.title("Analyse")
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = (screen_width / 2) - (gui_width / 2)
-y = (screen_height / 2) - (gui_height / 2)
-root.geometry("%dx%d+%d+%d" % (gui_width, gui_height, x, y))
-frame1 = Frame(root, width=600, height=350)
-frame2 = Frame(root, width=600, height=100)
-frame22 = Frame(root, width=600, height=300)
-frame3 = Frame(root, width=1200, height=300)
-frame1.place(x = 10, y =10 )
-frame2.place(x = 610, y =10 )
-frame22.place(x = 610, y =110)
-frame3.place(x = 10, y = 360)
-
-
-
-lbl_0 = Label(frame1, text='Отбор по базе данных')
-lbl_0.config(font=("Courier", 12))
-lbl_0.place( x =0, y = 0)
-lbl_1 = Label(frame2, text='Построение зависимостей')
-lbl_1.config(font=("Courier", 12))
-lbl_1.place(x = 0 , y =0)
-var_2 = DoubleVar()
-var_3 = DoubleVar()
-var_4 = DoubleVar()
-var_5 = DoubleVar()
-var_6 = DoubleVar()
-var_7 = DoubleVar()
-var_8 = DoubleVar()
-var_9 = DoubleVar()
-
-
-lbl_2 = Label(frame1, text="Average rating from")
-lbl_2.grid(row=1, column=1)
-sca_2= Scale(frame1, variable = var_2, orient=HORIZONTAL, length=150, from_=0, to=5, tickinterval=1, resolution=0.1)
-sca_2.grid(row=1, column=3)
-# btn_0 = Button(root, text="Get Scale Value", command=sel) #обработка значения курсора
-# btn_0.grid(row=3, column=6)
-lbl_3 = Label(frame1, text="to")
-lbl_3.grid(row=1, column=4)
-sca_3= Scale(frame1, variable = var_3, orient=HORIZONTAL, length=150, from_=0, to=5, tickinterval=1, resolution=0.1)
-sca_3.set(5.0)
-sca_3.grid(row=1, column=5)
-lbl_4 = Label(frame1, text="Number of pages from")
-lbl_4.grid(row=2, column=1)
-sca_4= Scale(frame1,variable = var_4, orient=HORIZONTAL, length=150, from_=0, to=6576, tickinterval=3288, resolution=1)
-sca_4.grid(row=2, column=3)
-lbl_5 = Label(frame1, text="to")
-lbl_5.grid(row=2, column=4)
-sca_5= Scale(frame1,variable = var_5, orient=HORIZONTAL, length=150, from_=0, to=6576, tickinterval=3288, resolution=1)
-sca_5.set(6576.0)
-sca_5.grid(row=2, column=5)
-lbl_6 = Label(frame1, text="Rating count from")
-lbl_6.grid(row=3, column=1)
-sca_6= Scale(frame1, variable = var_6, orient=HORIZONTAL, length=150, from_=0, to=250000, tickinterval=250000, resolution=1000)
-sca_6.grid(row=3, column=3)
-lbl_7 = Label(frame1, text="to")
-lbl_7.grid(row=3, column=4)
-sca_7= Scale(frame1, variable = var_7, orient=HORIZONTAL, length=150, from_=0,  to=250000, tickinterval=250000, resolution=1000)
-sca_7.set(4600000.0)
-sca_7.grid(row=3, column=5)
-lbl_8 = Label(frame1, text="Text reviews from")
-lbl_8.grid(row=4, column=1)
-sca_8= Scale(frame1,variable = var_8, orient=HORIZONTAL, length=150, from_=0, to=20000, tickinterval=20000, resolution=100)
-sca_8.grid(row=4, column=3)
-lbl_9 = Label(frame1, text="to")
-lbl_9.grid(row=4, column=4)
-sca_9= Scale(frame1,variable = var_9, orient=HORIZONTAL, length=150, from_=0, to=20000, tickinterval=20000, resolution=100)
-sca_9.set(94300)
-sca_9.grid(row=4, column=5)
-
-
-download_init_data_base = Button(frame1, text ='Скачать',command=download_main)
-download_init_data_base.grid(row=5, column=1)
-reload_data_base= Button(frame1, text='Обновить базу данных', command=update_table)
-reload_data_base.grid(row=5, column=2)
-
-values = ['Choose', 'average_rating','num_pages','ratings_count','text_reviews_count']
-
-
-combo_hist =ttk.Combobox(frame2,values = values)
-graph_label = Label(frame2, text ='График',font='bold')
-graph_label.place(x =0, y= 30)
-combo_plot1 = ttk.Combobox(frame2,values =values )
-combo_plot2 = ttk.Combobox(frame2,values =values )
-combo_plot1.place(x = 0, y = 60)
-combo_plot2.place(x = 200, y = 60)
-combo_plot1.current(0)
-combo_plot2.current(0)
-ttk.Button(frame2, text = 'PLOT' ,command =show).place(x = 60, y = 30)
-
-
-root.mainloop()
+#
+# root = Tk()
+# root.title("Analyse")
+# screen_width = root.winfo_screenwidth()
+# screen_height = root.winfo_screenheight()
+# x = (screen_width / 2) - (gui_width / 2)
+# y = (screen_height / 2) - (gui_height / 2)
+# root.geometry("%dx%d+%d+%d" % (gui_width, gui_height, x, y))
+# frame1 = Frame(root, width=600, height=350)
+# frame2 = Frame(root, width=600, height=100)
+# frame22 = Frame(root, width=600, height=300)
+# frame3 = Frame(root, width=1200, height=300)
+# frame1.place(x = 10, y =10 )
+# frame2.place(x = 610, y =10 )
+# frame22.place(x = 610, y =110)
+# frame3.place(x = 10, y = 360)
+#
+#
+#
+# lbl_0 = Label(frame1, text='Отбор по базе данных')
+# lbl_0.config(font=("Courier", 12))
+# lbl_0.place( x =0, y = 0)
+# lbl_1 = Label(frame2, text='Построение зависимостей')
+# lbl_1.config(font=("Courier", 12))
+# lbl_1.place(x = 0 , y =0)
+# var_2 = DoubleVar()
+# var_3 = DoubleVar()
+# var_4 = DoubleVar()
+# var_5 = DoubleVar()
+# var_6 = DoubleVar()
+# var_7 = DoubleVar()
+# var_8 = DoubleVar()
+# var_9 = DoubleVar()
+#
+#
+# lbl_2 = Label(frame1, text="Average rating from")
+# lbl_2.grid(row=1, column=1)
+# sca_2= Scale(frame1, variable = var_2, orient=HORIZONTAL, length=150, from_=0, to=5, tickinterval=1, resolution=0.1)
+# sca_2.grid(row=1, column=3)
+#
+# lbl_3 = Label(frame1, text="to")
+# lbl_3.grid(row=1, column=4)
+# sca_3= Scale(frame1, variable = var_3, orient=HORIZONTAL, length=150, from_=0, to=5, tickinterval=1, resolution=0.1)
+# sca_3.set(5.0)
+# sca_3.grid(row=1, column=5)
+# lbl_4 = Label(frame1, text="Number of pages from")
+# lbl_4.grid(row=2, column=1)
+# sca_4= Scale(frame1,variable = var_4, orient=HORIZONTAL, length=150, from_=0, to=6576, tickinterval=3288, resolution=1)
+# sca_4.grid(row=2, column=3)
+# lbl_5 = Label(frame1, text="to")
+# lbl_5.grid(row=2, column=4)
+# sca_5= Scale(frame1,variable = var_5, orient=HORIZONTAL, length=150, from_=0, to=6576, tickinterval=3288, resolution=1)
+# sca_5.set(6576.0)
+# sca_5.grid(row=2, column=5)
+# lbl_6 = Label(frame1, text="Rating count from")
+# lbl_6.grid(row=3, column=1)
+# sca_6= Scale(frame1, variable = var_6, orient=HORIZONTAL, length=150, from_=0, to=250000, tickinterval=250000, resolution=1000)
+# sca_6.grid(row=3, column=3)
+# lbl_7 = Label(frame1, text="to")
+# lbl_7.grid(row=3, column=4)
+# sca_7= Scale(frame1, variable = var_7, orient=HORIZONTAL, length=150, from_=0,  to=250000, tickinterval=250000, resolution=1000)
+# sca_7.set(4600000.0)
+# sca_7.grid(row=3, column=5)
+# lbl_8 = Label(frame1, text="Text reviews from")
+# lbl_8.grid(row=4, column=1)
+# sca_8= Scale(frame1,variable = var_8, orient=HORIZONTAL, length=150, from_=0, to=20000, tickinterval=20000, resolution=100)
+# sca_8.grid(row=4, column=3)
+# lbl_9 = Label(frame1, text="to")
+# lbl_9.grid(row=4, column=4)
+# sca_9= Scale(frame1,variable = var_9, orient=HORIZONTAL, length=150, from_=0, to=20000, tickinterval=20000, resolution=100)
+# sca_9.set(94300)
+# sca_9.grid(row=4, column=5)
+#
+#
+# download_init_data_base = Button(frame1, text ='Скачать',command=download_main)
+# download_init_data_base.grid(row=5, column=1)
+# reload_data_base= Button(frame1, text='Обновить базу данных', command=update_table)
+# reload_data_base.grid(row=5, column=2)
+#
+# values = ['Choose', 'average_rating','num_pages','ratings_count','text_reviews_count']
+#
+#
+# combo_hist =ttk.Combobox(frame2,values = values)
+# graph_label = Label(frame2, text ='График',font='bold')
+# graph_label.place(x =0, y= 30)
+# combo_plot1 = ttk.Combobox(frame2,values =values )
+# combo_plot2 = ttk.Combobox(frame2,values =values )
+# combo_plot1.place(x = 0, y = 60)
+# combo_plot2.place(x = 200, y = 60)
+# combo_plot1.current(0)
+# combo_plot2.current(0)
+# ttk.Button(frame2, text = 'PLOT' ,command =show).place(x = 60, y = 30)
+#
+#
+# root.mainloop()
